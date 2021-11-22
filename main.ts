4e001cae088b7c1304abaea6889bfd69326d8235
Nanogamexe <47512743+Nanogamexe@users.noreply.github.com>
2021-11-22 10:20:15 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
-let nombre = 0
-let etat_passe = 0
-let etat_actuel = 0
+let faiseau = 0
+let active = 0
 basic.forever(function () {
-    etat_actuel = pins.digitalReadPin(DigitalPin.P16)
-    if (etat_actuel != etat_passe) {
-        if (etat_actuel == 1) {
-            nombre += 1
-        }
+    faiseau = pins.digitalReadPin(DigitalPin.P16)
+    if (faiseau == 1) {
+        faiseau = active
     }
-    etat_passe = etat_actuel
-    basic.showNumber(nombre)
+    if (faiseau == active) {
+        basic.showIcon(IconNames.Ghost)
+    }
 })
